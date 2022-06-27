624701ad4b8235e673e24f654946bd77545b57a6
Charles Martins <charles.marttins@gmail.com>
2022-06-27 17:04:00 -0400

    Versão final

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 let insertName = prompt("Qual o seu nome?");
 insertName = {
     name: insertName
@@ -12,8 +13,23 @@ connectInTheRoom.catch(function(){
     insertName.name = prompt("Este nome já está em uso, por favor insira novamente!")
 })
 const idInterval = setInterval(connection, 4000);
+
 searchMessages()
 
+function hidePrivateMessages (answer){
+    if (answer.to === "Todos") {
+        return true;
+    }
+    if (answer.type === "private_message" && (answer.from === insertName.name || answer.to === insertName.name)){
+        return true;
+    }
+    if (answer.type === "status" || answer.type === "message"){
+        return true;
+    }
+    return false;
+
+
+}
 function searchMessages (){
 const promise =  axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
 promise.then(listMessages)
@@ -22,48 +38,44 @@ function listMessages (element){
 const messageDistribution = document.querySelector(".messages")
 messageDistribution.innerHTML = "";
 for (let i = 0; i < element.data.length; i++){
-    if (element.data[i].type === "status"){
+    const answer = element.data[i];
+    if (answer.type === "status"){
         messageDistribution.innerHTML += `
         <li class="connection-status">
-        <span class="message-time">(${element.data[i].time})</span>
-        <strong>${element.data[i].from}</strong>
-        <span>${element.data[i].text}</span>
+        <span class="message-time">(${answer.time})</span>
+        <strong>&nbsp${answer.from}&nbsp</strong>
+        <span>&nbsp${answer.text}&nbsp</span>
         </li>
         `;
         
     }
-    else if(element.data[i].type === "message" ){
+    else if(answer.type === "message" ){
         messageDistribution.innerHTML += `
         <li class="public-message">
-        <span class="message-time">(${element.data[i].time})</span>
-        <strong>${element.data[i].from}</strong>
-        <span> para </span>
-        <strong>${element.data[i].to}:</strong>
-        <span>${element.data[i].text}</span>
+        <span class="message-time">(${answer.time})</span>
+        <strong> ${answer.from} </strong>
+        <span>&nbsppara&nbsp</span>
+        <strong>&nbsp${answer.to}:&nbsp</strong>
+        <span>&nbsp${answer.text}&nbsp</span>
         </li>
         `;
 
 
     }
-    else if (element.data[i].type === "private_message"){
+    else if (hidePrivateMessages(answer)){
         messageDistribution.innerHTML += `
         <li class="private-messages">
-        <span class="message-time">(${element.data[i].time})</span>
-        <strong>${element.data[i].from}</strong>
-        <span> reservadamente para </span>
-        <strong>${element.data[i].to}:</strong>
-        <span>${element.data[i].text}</span>
+        <span class="message-time">(${answer.time})</span>
+        <strong>&nbsp${answer.from}&nbsp</strong>
+        <span>&nbspreservadamente para&nbsp</span>
+        <strong>&nbsp${answer.to}:&nbsp</strong>
+        <span>&nbsp${answer.text}&nbsp</span>
         </li>
         `;
     }
 }
 showMessages();
 
-}
-
-function showMessages (){
-    const containerMessages = document.querySelector(".container");
-    containerMessages.lastElementChild.scrollIntoView();
 }
 
 function sendMessages () {
@@ -84,3 +96,9 @@ function sendMessages () {
     showMessages();
 }
 setInterval(searchMessages, 3000);
+
+
+function showMessages (){
+    const containerMessages = document.querySelector(".messages");
+    containerMessages.lastElementChild.scrollIntoView();
+}
